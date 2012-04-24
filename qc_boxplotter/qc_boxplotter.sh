ab69510d06cdcc4b6172ac630d7d9659e4faf141
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-04-24 13:35:20 +0100

    qc_boxplotter tool: updated to version 0.0.3 (capture PNG output as well as PDF).

--- qc_boxplotter/qc_boxplotter.sh
+++ qc_boxplotter/qc_boxplotter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -fe
 #
-# usage: sh qc_boxplotter.sh <qual_in> <seq_order_boxplot_pdf_out>
+# usage: sh qc_boxplotter.sh <qual_in> <seq_order_boxplot_pdf_out> <seq_order_boxplot_png_out>
 #
 echo QC Boxplotter: generate QC boxplot
 #
@@ -8,14 +8,22 @@ echo QC Boxplotter: generate QC boxplot
 qc_boxplotter.sh $1
 #
 # Outputs are <qual_in>_seq-order_boxplot.pdf and
-# <qual_in>_primer-order_boxplot.pdf
+# <qual_in>_seq-order_boxplot.png
 #
 # check these exist and rename to supplied arguments
-if [ -f "${1}_seq-order_boxplot.pdf" ] ; then
-    echo Move ${1}_seq-order_boxplot.pdf to $2
-    /bin/mv ${1}_seq-order_boxplot.pdf $2
+boxplot_pdf=${1}_seq-order_boxplot.pdf
+if [ -f "$boxplot_pdf" ] ; then
+    echo Move $boxplot_pdf to $2
+    /bin/mv $boxplot_pdf $2
 else
-    echo Can\'t find ${1}_seq-order_boxplot.pdf
+    echo Can\'t find $boxplot_pdf
+fi
+boxplot_png=${1}_seq-order_boxplot.png
+if [ -f "$boxplot_png" ] ; then
+    echo Move $boxplot_png to $3
+    /bin/mv $boxplot_png $3
+else
+    echo Can\'t find $boxplot_png
 fi
 #
 # There are also corresponding .ps files that we'll delete
