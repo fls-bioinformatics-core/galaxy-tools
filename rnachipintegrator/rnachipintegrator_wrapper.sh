33bfa077a58563c01b16a96186b8dfa1d87ee8b9
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-04-26 14:06:12 +0100

    rnachipintegrator_wrapper: clean up script internals & keep program stdout.

--- rnachipintegrator/rnachipintegrator_wrapper.sh
+++ rnachipintegrator/rnachipintegrator_wrapper.sh
@@ -2,7 +2,7 @@
 #
 # Wrapper script to run RnaChipIntegrator as a Galaxy tool
 #
-# usage: sh rnachipintegrator_wrapper.sh [OPTIONS] <rnaseq_in> <chipseq_in> --output <xls_out>
+# usage: sh rnachipintegrator_wrapper.sh [OPTIONS] <rnaseq_in> <chipseq_in> --output_xls <xls_out>
 #
 echo RnaChipIntegrator: analyse gene expression and ChIP data
 #
@@ -10,7 +10,7 @@ echo RnaChipIntegrator: analyse gene expression and ChIP data
 opts=
 output_xls=
 while [ ! -z "$1" ] ; do
-    if [ "$1" == "--output" ] ; then
+    if [ "$1" == "--output_xls" ] ; then
 	shift; output_xls=$1
     else
 	opts="$opts $1"
@@ -21,20 +21,16 @@ done
 # Run RnaChipIntegrator
 # NB append stderr to stdout otherwise Galaxy job will fail
 # Direct output to a temporary directory
-log=`mktemp`
 outdir=`mktemp -d`
 base_name=galaxy
 cmd="RnaChipIntegrator.py --project=${outdir}/${base_name} $opts"
 echo $cmd
-$cmd > $log 2>&1
+$cmd 2>&1
 #
 # Check exit code
 if [ "$?" -ne "0" ] ; then
     echo RnaChipIntegrator exited with non-zero status
-    echo Output:
-    cat $log
     # Clean up and exit
-    /bin/rm -f $log
     /bin/rm -rf $outdir
     exit $?
 fi
@@ -44,15 +40,12 @@ if [ -f "${outdir}/${base_name}.xls" ] ; then
     /bin/mv ${outdir}/${base_name}.xls $output_xls
 else
     echo No file ${outdir}/${base_name}.xls
-    cat $log
     # Clean up and exit
-    /bin/rm -f $log
     /bin/rm -rf $outdir
     exit 1
 fi
 #
 # Clean up
-/bin/rm -f $log
 /bin/rm -rf $outdir
 #
 # Done
