8bc407292a79ebf2f9eae8afcab4584c5902e705
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-04-26 13:58:36 +0100

    rnachipintegrator_wrapper: start adding options when input data are summits (--window).

--- rnachipintegrator/rnachipintegrator_wrapper.sh
+++ rnachipintegrator/rnachipintegrator_wrapper.sh
@@ -2,9 +2,21 @@
 #
 # Wrapper script to run RnaChipIntegrator as a Galaxy tool
 #
-# usage: sh rnachipintegrator_wrapper.sh <rnaseq_in> <chipseq_in> <xls_out>
+# usage: sh rnachipintegrator_wrapper.sh [OPTIONS] <rnaseq_in> <chipseq_in> --output <xls_out>
 #
 echo RnaChipIntegrator: analyse gene expression and ChIP data
+#
+# Collect command line options
+opts=
+output_xls=
+while [ ! -z "$1" ] ; do
+    if [ "$1" == "--output" ] ; then
+	shift; output_xls=$1
+    else
+	opts="$opts $1"
+    fi
+    shift
+done 
 #
 # Run RnaChipIntegrator
 # NB append stderr to stdout otherwise Galaxy job will fail
@@ -12,7 +24,9 @@ echo RnaChipIntegrator: analyse gene expression and ChIP data
 log=`mktemp`
 outdir=`mktemp -d`
 base_name=galaxy
-RnaChipIntegrator.py --project=${outdir}/${base_name} $1 $2 > $log 2>&1
+cmd="RnaChipIntegrator.py --project=${outdir}/${base_name} $opts"
+echo $cmd
+$cmd > $log 2>&1
 #
 # Check exit code
 if [ "$?" -ne "0" ] ; then
@@ -27,7 +41,7 @@ fi
 #
 # Only output the XLS file for now
 if [ -f "${outdir}/${base_name}.xls" ] ; then
-    /bin/mv ${outdir}/${base_name}.xls $3
+    /bin/mv ${outdir}/${base_name}.xls $output_xls
 else
     echo No file ${outdir}/${base_name}.xls
     cat $log
