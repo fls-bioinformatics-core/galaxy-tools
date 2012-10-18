913637608d19ef7ec13725bea20802556da93460
Peter Briggs <peter.briggs@manchester.ac.uk>
2012-10-18 12:11:08 +0100

    qc_boxplotter: update to operate in current working directory (otherwise tries to operate in the same directory as the inpuy QUAL file).

--- qc_boxplotter/qc_boxplotter.sh
+++ qc_boxplotter/qc_boxplotter.sh
@@ -4,21 +4,25 @@
 #
 echo QC Boxplotter: generate QC boxplot
 #
+# Make a link to the input file in the current directory
+ln -s $1
+qual=`basename $1`
+#
 # Run qc_boxplotter
-qc_boxplotter.sh $1
+qc_boxplotter.sh $qual
 #
 # Outputs are <qual_in>_seq-order_boxplot.pdf and
 # <qual_in>_seq-order_boxplot.png
 #
 # check these exist and rename to supplied arguments
-boxplot_pdf=${1}_seq-order_boxplot.pdf
+boxplot_pdf=${qual}_seq-order_boxplot.pdf
 if [ -f "$boxplot_pdf" ] ; then
     echo Move $boxplot_pdf to $2
     /bin/mv $boxplot_pdf $2
 else
     echo Can\'t find $boxplot_pdf
 fi
-boxplot_png=${1}_seq-order_boxplot.png
+boxplot_png=${qual}_seq-order_boxplot.png
 if [ -f "$boxplot_png" ] ; then
     echo Move $boxplot_png to $3
     /bin/mv $boxplot_png $3
@@ -27,8 +31,8 @@ else
 fi
 #
 # There are also corresponding .ps files that we'll delete
-if [ -f "${1}_seq-order_boxplot.ps" ] ; then
-    /bin/rm -f ${1}_seq-order_boxplot.ps
+if [ -f "${qual}_seq-order_boxplot.ps" ] ; then
+    /bin/rm -f ${qual}_seq-order_boxplot.ps
 fi
 #
 # Done
