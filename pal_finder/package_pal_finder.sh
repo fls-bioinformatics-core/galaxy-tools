f941c3fc46ed695b76afeed0532b35581e0d36b1
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-09-17 15:31:51 +0100

    pal_finder: add packager script to build tgz file for upload to toolshed.

---
+++ pal_finder/package_pal_finder.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Package pal_finder tool files into tgz file for upload to
+# Galaxy toolshed
+#
+TGZ=pal_finder.tgz
+if [ -f $TGZ ] ; then
+    echo $TGZ: already exists, please remove >&2
+    exit 1
+fi
+tar cvzf $TGZ \
+    README.markdown \
+    pal_finder_wrapper.sh \
+    pal_finder_wrapper.xml \
+    tool_dependencies.xml \
+    test-data
+if [ -f $TGZ ] ; then
+    echo Created $TGZ
+else
+    echo Failed to created $TGZ >&2
+    exit 1
+fi
+##
+#
