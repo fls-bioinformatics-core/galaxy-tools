bed002d6859e81228542cc2390197c47e58aae5f
Peter Briggs <peter.briggs@manchester.ac.uk>
2014-09-19 09:53:32 +0100

    fastq_screen: add 'packager' script to make .tgz file for upload to tool shed.

---
+++ fastq_screen/package_fastq_screen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# Package fastq_screen tool files into tgz file for upload to
+# Galaxy toolshed
+#
+TGZ=fastq_screen.tgz
+if [ -f $TGZ ] ; then
+    echo $TGZ: already exists, please remove >&2
+    exit 1
+fi
+tar cvzf $TGZ \
+    README.markdown \
+    fastq_screen.xml \
+    fastq_screen.sh \
+    tool-data/fastq_screen.loc.sample \
+    tool_dependencies.xml
+if [ -f $TGZ ] ; then
+    echo Created $TGZ
+else
+    echo Failed to created $TGZ >&2
+    exit 1
+fi
+##
+#
