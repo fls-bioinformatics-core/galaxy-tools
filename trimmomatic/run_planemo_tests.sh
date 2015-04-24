45b89d35d163a30a5310cd550e1cb58199cf540c
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-04-24 11:10:10 +0100

    Updates to Travis and planemo based tests, to work with an existing Galaxy installation.

--- trimmomatic/run_planemo_tests.sh
+++ trimmomatic/run_planemo_tests.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 #
 # Install dependencies and set up environment for
-# trimmomatic tool
+# trimmomatic tool, then run tests using planemo
 #
-INSTALL_DIR=$(pwd)/tool_dependencies/trimmomatic/0.32
+# Note that any arguments supplied to the script are
+# passed directly to the "planemo test..." invocation
+#
+# e.g. --install_galaxy (to get planemo to create a
+#                        Galaxy instance to run tests)
+#
+#      --galaxy_root DIR (to run tests using existing
+#                         Galaxy instance)
+#
+INSTALL_DIR=$(pwd)/test.tool_dependencies/trimmomatic
 if [ -e $INSTALL_DIR ] ; then
-    echo $INSTALL_DIR already exists >&2
-    exit 1
+    echo WARNING $INSTALL_DIR already exists >&2
+else
+    # Download and install Trimmomatic
+    mkdir -p $INSTALL_DIR
+    wd=$(mktemp -d)
+    echo Moving to $wd
+    pushd $wd
+    wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.32.zip
+    unzip Trimmomatic-0.32.zip
+    mv Trimmomatic-0.32/trimmomatic-0.32.jar $INSTALL_DIR/
+    mv Trimmomatic-0.32/adapters/ $INSTALL_DIR/
+    popd
+    rm -rf $wd/*
+    rmdir $wd
 fi
-mkdir -p $INSTALL_DIR
-#
-# Download and install Trimmomatic
-wd=$(mktemp -d)
-echo Moving to $wd
-pushd $wd
-wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.32.zip
-unzip Trimmomatic-0.32.zip
-mv Trimmomatic-0.32/trimmomatic-0.32.jar $INSTALL_DIR/
-mv Trimmomatic-0.32/adapters/ $INSTALL_DIR/
-popd
-#rm -rf $wd/*
-#rmdir $wd
 # Set up the environment
 export TRIMMOMATIC_DIR=$INSTALL_DIR
 export TRIMMOMATIC_ADAPTERS_DIR=$INSTALL_DIR/adapters
-# Run the planemo testsls
-planemo test --install_galaxy $(dirname $0)/trimmomatic.xml
+# Run the planemo tests
+planemo test $@ $(dirname $0)/trimmomatic.xml
 ##
 #
