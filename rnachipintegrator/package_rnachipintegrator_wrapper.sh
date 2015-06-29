4b1154152239489a2e4c0ce2292f1fc050da5fd4
Peter Briggs <peter.briggs@manchester.ac.uk>
2015-06-29 14:12:26 +0100

    rnachipintegrator: add a macro files to share requirements etc between different tools.

--- rnachipintegrator/package_rnachipintegrator_wrapper.sh
+++ rnachipintegrator/package_rnachipintegrator_wrapper.sh
@@ -3,14 +3,15 @@
 # Package RnaChipIntegrator files into tgz file for upload to
 # Galaxy toolshed
 #
-VERSION=$(grep "^<tool" rnachipintegrator_wrapper.xml | grep -o -e version=\".*\" | cut -d= -f2 | tr -d \")
-TGZ=rnachipintegrator_wrapper-${VERSION}.tgz
+VERSION=$(grep "@VERSION@" rnachipintegrator_macros.xml | cut -d'>' -f2 | cut -d'<' -f1)
+TGZ=rnachipintegrator-${VERSION}.tgz
 if [ -f $TGZ ] ; then
     echo $TGZ: already exists, please remove >&2
     exit 1
 fi
 tar cvzf $TGZ \
     README.rst \
+    rnachipintegrator_macros.xml \
     rnachipintegrator_wrapper.xml \
     rnachipintegrator_canonical_genes.xml \
     rnachipintegrator_wrapper.sh \
