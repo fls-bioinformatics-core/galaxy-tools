f0a376609cdc4756f4b5a392339ee34dd951f03d
Peter Briggs <peter.briggs@manchester.ac.uk>
2016-02-03 13:14:46 +0000

    rnachipintegrator: update to install and test using v0.5.0-alpha.7.

--- rnachipintegrator/install_tool_deps.sh
+++ rnachipintegrator/install_tool_deps.sh
@@ -15,8 +15,8 @@ if [ ! -d "$TOP_DIR" ] ; then
     mkdir -p $TOP_DIR
 fi
 cd $TOP_DIR
-# RnaChipIntegrator 0.5.0-alpha.6
-VERSION=0.5.0-alpha.6
+# RnaChipIntegrator 0.5.0-alpha.7
+VERSION=0.5.0-alpha.7
 INSTALL_DIR=$TOP_DIR/rnachipintegrator/$VERSION
 mkdir -p $INSTALL_DIR
 wd=$(mktemp -d)
@@ -36,14 +36,14 @@ export PATH=$INSTALL_DIR/bin:\$PATH
 export PYTHONPATH=$INSTALL_DIR/lib/python2.7/site-packages:\$PYTHONPATH
 #
 EOF
-# xlwt 0.7.5
-INSTALL_DIR=$TOP_DIR/xlwt/0.7.5
+# xlsxwriter 0.8.4
+INSTALL_DIR=$TOP_DIR/xlsxwriter/0.8.4
 mkdir -p $INSTALL_DIR
 wd=$(mktemp -d)
 pushd $wd
-wget -q https://pypi.python.org/packages/source/x/xlwt/xlwt-0.7.5.tar.gz
-tar xzf xlwt-0.7.5.tar.gz
-cd xlwt-0.7.5
+wget -q https://pypi.python.org/packages/source/X/XlsxWriter/XlsxWriter-0.8.4.tar.gz
+tar xzf XlsxWriter-0.8.4.tar.gz
+cd XlsxWriter-0.8.4
 OLD_PYTHONPATH=$PYTHONPATH
 mkdir -p $INSTALL_DIR/lib/python
 export PYTHONPATH=$PYTHONPATH:$INSTALL_DIR/lib/python
@@ -52,58 +52,10 @@ popd
 rm -rf $wd/*
 rmdir $wd
 export PYTHONPATH=$OLD_PYTHONPATH
-cat > xlwt/0.7.5/env.sh <<EOF
+cat > xlsxwriter/0.8.4/env.sh <<EOF
 #!/bin/sh
-# Source this to setup xlwt/0.7.5
-echo Setting up xlwt 0.7.5
-export PYTHONPATH=$INSTALL_DIR/lib/python:\$PYTHONPATH
-export PATH=$INSTALL_DIR/bin:\$PATH
-#
-EOF
-# xlrd 0.9.3
-INSTALL_DIR=$TOP_DIR/xlrd/0.9.3
-mkdir -p $INSTALL_DIR
-wd=$(mktemp -d)
-pushd $wd
-wget -q https://pypi.python.org/packages/source/x/xlrd/xlrd-0.9.3.tar.gz
-tar xzf xlrd-0.9.3.tar.gz
-cd xlrd-0.9.3
-OLD_PYTHONPATH=$PYTHONPATH
-mkdir -p $INSTALL_DIR/lib/python
-export PYTHONPATH=$PYTHONPATH:$INSTALL_DIR/lib/python
-python setup.py install --install-lib $INSTALL_DIR/lib/python --install-scripts $INSTALL_DIR/bin
-popd
-rm -rf $wd/*
-rmdir $wd
-export PYTHONPATH=$OLD_PYTHONPATH
-cat > xlrd/0.9.3/env.sh <<EOF
-#!/bin/sh
-# Source this to setup xlrd/0.9.3
-echo Setting up xlrd 0.9.3
-export PYTHONPATH=$INSTALL_DIR/lib/python:\$PYTHONPATH
-export PATH=$INSTALL_DIR/bin:\$PATH
-#
-EOF
-# xlutils 1.7.1
-INSTALL_DIR=$TOP_DIR/xlutils/1.7.1
-mkdir -p $INSTALL_DIR
-wd=$(mktemp -d)
-pushd $wd
-wget -q https://pypi.python.org/packages/source/x/xlutils/xlutils-1.7.1.tar.gz
-tar xzf xlutils-1.7.1.tar.gz
-cd xlutils-1.7.1
-OLD_PYTHONPATH=$PYTHONPATH
-mkdir -p $INSTALL_DIR/lib/python
-export PYTHONPATH=$PYTHONPATH:$INSTALL_DIR/lib/python
-python setup.py install --install-lib $INSTALL_DIR/lib/python --install-scripts $INSTALL_DIR/bin
-popd
-rm -rf $wd/*
-rmdir $wd
-export PYTHONPATH=$OLD_PYTHONPATH
-cat > xlutils/1.7.1/env.sh <<EOF
-#!/bin/sh
-# Source this to setup xlutils/1.7.1
-echo Setting up xlutils 1.7.1
+# Source this to setup xlsxwriter/0.8.4
+echo Setting up xlsxwriter 0.8.4
 export PYTHONPATH=$INSTALL_DIR/lib/python:\$PYTHONPATH
 export PATH=$INSTALL_DIR/bin:\$PATH
 #
