07833db04d4dd5e3d83cfffdc8bab8a7c6876d3d
Peter van Heusden <pvh@sanbi.ac.za>
2017-01-20 13:29:22 +0200

    Work towards fastq.gz support

--- tools/trimmomatic/trimmomatic.sh
+++ tools/trimmomatic/trimmomatic.sh
@@ -5,7 +5,22 @@ echo Arguments:
 for i in $@ ; do
     echo "*" $i
 done
-java $@ 2>&1 | tee trimmomatic.log
+
+if [ -n "$CONDA_PREFIX" ] ; then
+  # bioconda installs trimmomatic in a share directory
+  # with a jarfile called trimmomatic.jar
+  TRIMMOMATIC_DIR="$CONDA_PREFIX/share/trimmomatic"
+  JARFILE="trimmomatic.jar"
+else
+  JARFILE="trimmomatic-0.36.jar"
+fi
+
+if [ -z "$TRIMMOMATIC_DIR" ] ; then
+  echo "TRIMMOMATIC_DIR variable not set, can't find jar file"
+  exit 1
+fi
+
+java -mx8G -jar $TRIMMOMATIC_DIR/$JARFILE $@ 2>&1 | tee trimmomatic.log
 status=$?
 echo "Exit status: $status"
 # Check for successful completion
