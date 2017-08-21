58004ad46690d7a6215c858a9345e03a9d45aa48
Peter Briggs <peter.briggs@manchester.ac.uk>
2017-08-21 10:12:38 +0100

    Update trimmomatic packager script to include version in tgz output.

--- tools/trimmomatic/package_trimmomatic.sh
+++ tools/trimmomatic/package_trimmomatic.sh
@@ -3,7 +3,8 @@
 # Package Trimmomatic tool files into tgz file for upload to
 # Galaxy toolshed
 #
-TGZ=trimmomatic.tgz
+VERSION=$(grep "^<tool" trimmomatic.xml | grep -o -e version=\".*\" | cut -d= -f2 | tr -d \")
+TGZ=trimmomatic-${VERSION}.tgz
 if [ -f $TGZ ] ; then
     echo $TGZ: already exists, please remove >&2
     exit 1
