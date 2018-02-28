01181b76835cfa958d81b17d330eea059aa15ede
Peter Briggs <peter.briggs@manchester.ac.uk>
2018-02-28 15:54:04 +0000

    trimmomatic: always use conda dependency resolution from now on.

--- tools/trimmomatic/run_planemo_tests.sh
+++ tools/trimmomatic/run_planemo_tests.sh
@@ -12,27 +12,6 @@
 #      --galaxy_root DIR (to run tests using existing
 #                         Galaxy instance)
 #
-# List of dependencies
-TOOL_DEPENDENCIES="trimmomatic/0.36"
-# Where to find them
-TOOL_DEPENDENCIES_DIR=$(pwd)/test.tool_dependencies.trimmomatic
-if [ ! -d $TOOL_DEPENDENCIES_DIR ] ; then
-    echo WARNING $TOOL_DEPENDENCIES_DIR not found >&2
-    echo Creating tool dependencies dir
-    mkdir -p $TOOL_DEPENDENCIES_DIR
-    echo Installing tool dependencies
-    $(dirname $0)/install_tool_deps.sh $TOOL_DEPENDENCIES_DIR
-fi
-# Load dependencies
-for dep in $TOOL_DEPENDENCIES ; do
-    env_file=$TOOL_DEPENDENCIES_DIR/$dep/env.sh
-    if [ -e $env_file ] ; then
-	. $env_file
-    else
-	echo ERROR no env.sh file found for $dep >&2
-	exit 1
-    fi
-done
 # Run the planemo tests
 planemo test $@ $(dirname $0)/trimmomatic.xml
 ##
