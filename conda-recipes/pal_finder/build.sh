27bbfdf8c313203f33e45c98b5c2acbcc68bd8c1
Peter Briggs <peter.briggs@manchester.ac.uk>
2018-03-02 15:11:15 +0000

    pal_finder: update conda recipe to keep version number in Perl script name.

--- conda-recipes/pal_finder/build.sh
+++ conda-recipes/pal_finder/build.sh
@@ -4,13 +4,12 @@ outdir=$PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM
 mkdir -p $outdir
 ln -s $PREFIX/share/$PKG_NAME-$PKG_VERSION-$PKG_BUILDNUM $PREFIX/share/$PKG_NAME
 
-files="config.txt COPYING.txt pal_finder_v0.02.04.pl README.txt simple.ref"
+files="config.txt COPYING.txt pal_finder_v${PKG_VERSION}.pl README.txt simple.ref"
 for f in $files ; do
     cp ./$f $outdir/
 done
-mv $outdir/pal_finder_v$PKG_VERSION.pl $outdir/pal_finder
-sed -i 's,^#!/usr/bin/perl,#!/usr/bin/env perl,g' $outdir/pal_finder
-chmod +x $outdir/pal_finder
+sed -i 's,^#!/usr/bin/perl,#!/usr/bin/env perl,g' $outdir/pal_finder_v${PKG_VERSION}.pl
+chmod +x $outdir/pal_finder_v${PKG_VERSION}.pl
 
 mkdir -p $PREFIX/bin
-ln -s $outdir/pal_finder $PREFIX/bin
+ln -s $outdir/pal_finder_v${PKG_VERSION}.pl $PREFIX/bin/pal_finder
