8387eca7f0d59f2ef03d74119165cca20ed72907
Peter Briggs <peter.briggs@manchester.ac.uk>
2018-03-06 11:59:05 +0000

    palfinder: update the conda recipe for primer3/2.0.0a.

--- conda-recipes/primer3/2.0.0a/build.sh
+++ conda-recipes/primer3/2.0.0a/build.sh
@@ -4,7 +4,14 @@ export CFLAGS="-I$PREFIX/include"
 export LDFLAGS="-L$PREFIX/lib"
 export CPATH=${PREFIX}/include
 
+mkdir -p $PREFIX/bin
 make -C src -f Makefile
 
-mkdir -p $PREFIX/bin
-cp src/primer3_core $PREFIX/bin
+binaries="\
+long_seq_tm_test \
+ntdpal \
+oligotm \
+primer3_core \
+"
+
+for i in $binaries; do cp src/$i $PREFIX/bin && chmod +x $PREFIX/bin/$i; done
