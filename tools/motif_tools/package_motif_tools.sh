e576391d74ee4d432bac5cdea4d275aa4cf387ac
Peter Briggs <peter.briggs@manchester.ac.uk>
2018-03-21 10:30:41 +0000

    motif_tools: add packager script for upload to toolsheds.

---
+++ tools/motif_tools/package_motif_tools.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Package motif_tools files into tgz file for upload to
+# Galaxy toolshed
+#
+VERSION=$(grep "@VERSION@" motif_tools_macros.xml | cut -d'>' -f2 | cut -d '<' -f1)
+TGZ=motif_tools-${VERSION}.tgz
+if [ -f $TGZ ] ; then
+    echo $TGZ: already exists, please remove >&2
+    exit 1
+fi
+tar cvzf $TGZ \
+    README.rst \
+    CountUniqueIDs.xml \
+    Scan_IUPAC_output_each_match.xml \
+    Scan_IUPAC_output_matches_per_seq.xml \
+    TFBScluster_candidates_2TFBS.xml \
+    TFBScluster_candidates_3TFBS.xml \
+    motif_tools_macros.xml \
+    test-data
+if [ -f $TGZ ] ; then
+    echo Created $TGZ
+else
+    echo Failed to created $TGZ >&2
+    exit 1
+fi
+##
+#
